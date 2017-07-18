505eb7bfb8a91ea5845360caa2590cfc7bcdc483
Ray Y Han <giraiday@gmail.com>
2017-07-17 22:10:57 -0700

    drag works

--- index.android.js
+++ index.android.js
@@ -9,86 +9,85 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  PanResponder,
+  Animated,
+  Dimensions,
+  TouchableHighlight
 } from 'react-native';
+import SortableListView from 'react-native-sortable-listview';
+import getData from './modules/get-data';
+
+let data = getData();
+let order = Object.keys(data);
 
 export default class JetspeedParkingLocator extends Component {
   constructor(props) {
     super(props);
-    this.data = [
-      {
-        color: 'blue',
-        name: 'Lot 10',
-        spaces: 20
-      },
-      {
-        color: 'blue',
-        name: 'Structure',
-        spaces: 17
-      },
-      {
-        color: 'red',
-        name: 'West',
-        spaces: 3
-      },
-      {
-        color: 'yellow',
-        name: 'hill',
-        spaces: 100
-      },
-      {
-        color: 'green',
-        name: 'underground',
-        spaces: 0
-      }
-    ];
+    this.rowClickHandler = this.rowClickHandler.bind(this);
+    this.state = {selected_row: null};
   }
+
+  rowClickHandler(name) {
+    let selected_row = this.state.selected_row;
+    selected_row && (data[selected_row]['is_selected'] = false);
+    this.setState({selected_row: name}, () => data[name]['is_selected'] = true);
+  }
+
   render() {
-    let views = this.data.map(lot => {
-      let view_style = {
-        backgroundColor: lot.color,
-        flex: 0.3,
-        margin: 5,
-        alignItems: 'center',
-        justifyContent: 'center'
-      };
-      let text_style = {
-        color: 'black',
-        fontSize: 30,
-        fontFamily: 'monospace',
-        fontWeight: 'bold'
-      };
-      return (
-        <View style={view_style} key={lot.name}>
-          <Text style={text_style} >{`${lot.name}: ${lot.spaces}`}</Text>
-        </View>
-      );
-    })
+    let rowClickHandler = this.rowClickHandler;
+    return <SortableListView
+      style={{flex: 1}}
+      data={data}
+      order={order}
+      rowHasChanged={() => this.forceUpdate()}
+      onRowMoved={e => {
+        order.splice(e.to, 0, order.splice(e.from, 1)[0]);
+        this.forceUpdate();
+      }}
+      renderRow={row => <SingleLotView data={row} row_clicker={rowClickHandler} />}
+    />
+  }
+}
+
+class SingleLotView extends Component {
+  render () {
+    let {color, name, spaces} = this.props.data;
+    let style = [styles.row_style, {backgroundColor: color}];
+    this.props.data.is_selected && style.push(styles.selected_row);
     return (
-      <View style={{flexDirection: 'column', flex: 1, padding: 20}}>
-        {views}
-      </View>
-    );
+      <TouchableHighlight
+        style={style}
+        delayLongPress={500}
+        underlayColor={'grey'}
+        {...this.props.sortHandlers}
+        onPress={() => {
+          this.props.row_clicker(name);
+        }}
+      >
+        <Text style={styles.text_style}>{`${name}: ${spaces}`}</Text>
+      </TouchableHighlight>
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
+  row_style: {
+    height: 100,
+    flexDirection: 'column',
+    margin: 5,
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  selected_row: {
+    height: 200
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  text_style: {
+    color: 'black',
+    fontSize: 30,
+    fontFamily: 'monospace',
+    fontWeight: 'bold'
+  }
 });
 
 AppRegistry.registerComponent('JetspeedParkingLocator', () => JetspeedParkingLocator);
---
+++ modules/get-data.js
@@ -0,0 +1,11 @@
+function getData () {
+  return {
+    lot_10: { color: 'blue', name: 'lot_10', spaces: 20 },
+    structure: { color: 'blue', name: 'structure', spaces: 17 },
+    west: { color: 'red', name: 'west', spaces: 3 },
+    hill: { color: 'yellow', name: 'hill', spaces: 100 },
+    underground: { color: 'green', name: 'underground', spaces: 0 }
+  };
+}
+
+module.exports = getData;
